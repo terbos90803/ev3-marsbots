2b30d172416455045fae66ea411d093e87b8734e
Terry <terbos90803@gmail.com>
2021-03-27 16:46:50 -0700

    First working REST api

--- marsbots-host/apihost.py
+++ marsbots-host/apihost.py
@@ -1,8 +1,8 @@
 import flask
-from flask import request, jsonify
+from flask import request, jsonify, make_response
 
 app = flask.Flask("MarsbotsServer")
-#app.config["DEBUG"] = True
+# app.config["DEBUG"] = True
 
 # Create some test data for our catalog in the form of a list of dictionaries.
 books = [
@@ -26,38 +26,51 @@ books = [
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>Shared Science Marsbot Adventure</h1>
+<p>Learn more at <a href="https://sharedscience.org/">Shared Science</a>.</p>'''
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
+@app.route('/robots', methods=['GET'])
+def get_valid_robots():
+    robots = {'status': 'ok', 'first_robot': '1', 'last_robot': '6'}
+    return jsonify(robots)
 
 
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
+@app.route('/robot', methods=['POST'])
+def register_robot():
+    if 'robot' in request.form:
+        robot = int(request.form['robot'])
+        # TODO validate robot id
+        if False:
+            app.logger.error(f'Duplicate robot registration:{robot}')
+            return {'status': 'fail', 'reason': 'Number already taken'}
+        return {'status': 'ok'}
     else:
-        return "Error: No id field provided. Please specify an id."
+        app.logger.error('Missing robot id for registration')
+        return {'status': 'fail', 'reason': 'Missing robot id'}
 
-    # Create an empty list for our results
-    results = []
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
+@app.route('/sol', methods=['GET'])
+def get_sol():
+    return {'status': 'ok', 'sol': '1.0', 'total_sols': '10.0', 'mins_per_sol': '3.0'}
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
+
+@app.route('/plan', methods=['POST'])
+def set_plan():
+    form_robot = request.form.get('robot')
+    if form_robot is not None:
+        robot = int(form_robot)
+        plan = request.form.get('plan')
+        if plan is not None:
+            # submit plan, get delay
+            app.logger.debug(f'plan:{request.form}')
+            return {'status': 'ok', 'delay': 10}
+        else:
+            # flag a rescue, get delay
+            app.logger.debug(f'rescue:{request.form}')
+            return {'status': 'ok', 'delay': 10}
+    return {'status': 'fail', 'reason': 'Missing robot id'}
+
 
 def run_server():
     app.run(host="0.0.0.0")
-
