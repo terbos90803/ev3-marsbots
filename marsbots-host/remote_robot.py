c3e87d434ebbf289c68833fe864ee3641561226a
Terry Wells <terbos@usa.net>
2021-05-08 19:39:18 -0700

    Automatically connect to IP robots

--- marsbots-host/remote_robot.py
+++ marsbots-host/remote_robot.py
@@ -11,12 +11,14 @@ class RemoteRobot:
     def __init__(self, robot_info):
         self.robot_ip_addr = None
         self.robot_mac_addr = robot_info['btmac']
+        self.heard_ip_ad = False
         self.s = None
 
     def connect(self):
         # Try TCP first
         if not self.s and self.robot_ip_addr:
             try:
+                self.heard_ip_ad = False
                 self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.s.connect((self.robot_ip_addr, _tcp_port))
                 print('Connected to robot', self.robot_ip_addr)
@@ -34,11 +36,16 @@ class RemoteRobot:
                 self.s = None
                 print(f'Failed to open BT connection to {self.robot_mac_addr}: {repr(err)}')
 
+    def heard_ad(self):
+        return self.heard_ip_ad
+
     def is_connected(self):
         return self.s is not None
 
     def set_ip(self, addr):
         self.robot_ip_addr = addr
+        self.heard_ip_ad = True
+        self.connect()
 
     def close(self):
         if self.s is not None:
